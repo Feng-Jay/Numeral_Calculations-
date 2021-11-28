95e7d66ef0555850759002027b4f3bdc7500a7d3
Feng-Jay <2652925120@qq.com>
2021-11-28 18:58:07 +0800

    python 真不戳啊

--- Lab1_Interpolation/Vandermonde.py
+++ Lab1_Interpolation/Vandermonde.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 class Vandermonde_Martix : 
-    n=0
-    def __init__(self):
-        self.n=1
+    martix=[]
+    def __init__(self, xn, *x):
+        x_set = x[0]
+        # print(x_set)
+        self.martix=[[x_set[j]**i for i in range(xn)] for j in range(xn)]
+        print(self.martix)
 
--- Lab1_Interpolation/outcome.py
+++ Lab1_Interpolation/outcome.py
@@ -2,21 +2,44 @@ from Vandermonde import Vandermonde_Martix
 import numpy as np
 import math
 
-array = input("Please input the Interval [a,b]: ")
-a = [float(n) for n in array.split()]
-array = input("Please input the value: c d e f to build the f(x): ")
-args = [float(n) for n in array.split()]
-n = int(input("Please input the value n+1: "))
-m = int(input("Please input the value m for test: "))
 
 
-c = args[0]
-d = args[1]
-e = args[2]
-f = args[3]
-x = math.pi/2
+def fx(x,*args):
+    arg=args[0]
+    # print(arg)
+    c = arg[0]
+    d = arg[1]
+    e = arg[2]
+    f = arg[3]
 
-fx = c * np.sin(d * x) + e * np.cos(f * x)
-print(fx)
+    fx = c * np.sin(d * x) + e * np.cos(f * x)
+    # print(fx)
+    return fx
+
+
+
+if __name__ == "__main__":
+    array = input("Please input the Interval [a,b]: ")
+    a = [float(n) for n in array.split()]
+    array = input("Please input the value: c d e f to build the f(x): ")
+    args = [float(n) for n in array.split()]
+    n = int(input("Please input the value n+1: "))
+    m = int(input("Please input the value m for test: "))
+    x_set = [] # n+1个插值点
+    y_set = [] # 插值点处y值
+    interval = a[1] - a [0] #输入区间的长度
+    pace = interval/n
+    # print(args)
+    # print(pace) #步长
+    for i in range(n):
+        x_set.append(a[0]+i*pace) #给定插值点x
+        y_set.append(fx(x_set[i],args))
+    # print(y_set)
+    # print(x_set)
+    # 获得所有插值点
+    # 准备构造插值函数
+    vander=Vandermonde_Martix(n,x_set)
+
+
 # print(a)
 # print(args)
